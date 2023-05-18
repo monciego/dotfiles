37739f991a88a094e768ae9e6aa667e64a159b4e
monciego <jerichobantiquete@gmail.com>
2023-05-19 02:26:38 +0800

    feat: add colorscheme

--- nvim/lua/monciego/packer.lua
+++ nvim/lua/monciego/packer.lua
@@ -15,7 +15,14 @@ return require('packer').startup(function(use)
   use("theprimeagen/harpoon")
   use("mbbill/undotree")
   use("tpope/vim-fugitive")
-  use 'folke/tokyonight.nvim'
+
+  use({
+      'rose-pine/neovim',
+      as = 'rose-pine',
+      config = function()
+          vim.cmd('colorscheme rose-pine')
+      end
+  })
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
