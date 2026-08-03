3f429ccbe6c72abe696a6a8f31ae0a8ec205dad7
monciego <jerichobantiquete@gmail.com>
2026-08-04 06:33:39 +0800

    feat: change background color

--- nvim/lua/plugins/colortheme.lua
+++ nvim/lua/plugins/colortheme.lua
@@ -1,32 +1,47 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    lazy = false,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha", -- latte, frappe, macchiato, mocha
-        transparent_background = false,
-        term_colors = true,
-        color_overrides = {
-          mocha = {
-            base = "#0d0e0e",
-            mantle = "#0d0e0e",
-            crust = "#0d0e0e",
-          },
-        },
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          telescope = true,
-          treesitter = true,
-          notify = false,
-          mini = false,
-        },
-      })
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				transparent_background = false,
+				term_colors = true,
+				color_overrides = {
+					mocha = {
+						base = "#1a1c1c",
+						mantle = "#1a1c1c",
+						crust = "#1a1c1c",
+					},
+				},
+				styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+					comments = { "italic" }, -- Change the style of comments
+					conditionals = { "italic" },
+					loops = {},
+					functions = {},
+					keywords = {},
+					strings = {},
+					variables = {},
+					numbers = {},
+					booleans = {},
+					properties = {},
+					types = {},
+					operators = {},
+					-- miscs = {}, -- Uncomment to turn off hard-coded styles
+				},
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					telescope = true,
+					treesitter = true,
+					notify = false,
+					mini = false,
+				},
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
