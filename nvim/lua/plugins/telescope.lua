f7cb28cddc6b360dbfa1a925249bf7b9c3b4bc19
monciego <jerichobantiquete@gmail.com>
2026-08-04 15:41:35 +0800

    chore: update telescope configuration

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 -- Fuzzy Finder (files, lsp, etc)
 return {
 	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
+	version = "*",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		-- Fuzzy Finder Algorithm which requires local dependencies to be built.
@@ -21,7 +21,6 @@ return {
 		"folke/todo-comments.nvim",
 	},
 	config = function()
-		local telescope = require("telescope")
 		local actions = require("telescope.actions")
 		local builtin = require("telescope.builtin")
 
