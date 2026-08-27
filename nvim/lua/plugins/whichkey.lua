dee10b9660922c6195e3026c2fb5d0a9efe0a31b
monciego <jerichobantiquete@gmail.com>
2026-08-27 23:33:19 +0800

    feat: add single border whichkey

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -6,6 +6,9 @@ return {
 		vim.o.timeoutlen = 500
 	end,
 	opts = {
+		win = {
+			border = "single",
+		},
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
